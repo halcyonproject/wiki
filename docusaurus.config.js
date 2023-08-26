f617705ea9d4da6585dc58299eeedfdbdb94bcac
slcee <slcee@proton.me>
2023-08-26 21:57:15 +0700

    config: Cleanup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,3 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
@@ -9,24 +6,13 @@ const config = {
   title: 'Halcyon Wiki',
   tagline: 'Welcome to the Halcyon Wiki!',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
   url: 'https://wiki.hlcyn.me',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'halcyonproject', // Usually your GitHub org/user name.
-  projectName: 'wiki', // Usually your repo name.
-
+  organizationName: 'halcyonproject',
+  projectName: 'wiki',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
